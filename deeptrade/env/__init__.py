5fa17886d780d462fba74a847be7866f1da2c5a2
quessy <aq15777@bristol.ac.uk>
2024-10-18 12:53:29 +0100

    removed pets_cartpole

--- deeptrade/env/__init__.py
+++ deeptrade/env/__init__.py
@@ -4,7 +4,7 @@ from .agents.hold import HoldAgent
 from .agents.ewmac import EWMACAgent
 from .agents.breakout import BreakoutAgent
 
-from . import reward_fns, termination_fns, pets_cartpole
+from . import reward_fns, termination_fns 
 import gymnasium as gym
 
 gym.register(
