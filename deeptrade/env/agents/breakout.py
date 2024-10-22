21827103a1067705c96f2091ae824fdf9eea4863
quessy <aq15777@bristol.ac.uk>
2024-10-22 16:08:24 +0100

    fixed breakout rule, no smoothing for now

--- deeptrade/env/agents/breakout.py
+++ deeptrade/env/agents/breakout.py
@@ -14,46 +14,43 @@ class BreakoutAgent:
         self._env = env
         self.lookback_period = lookback_period
 
+        # Setup smoothing
         if smooth is None:
             smooth = max(int(lookback_period / 4.0), 1)
-
         assert smooth < lookback_period, "Smooth must be less than lookback period."
         self.smooth = smooth
 
-        def calculate_rolling_extremes(self, prices: np.ndarray):
-            lookback = self.lookback_period
-            min_periods = int(min(len(prices), np.ceil(lookback / 2.0)))
+    def calculate_rolling_extremes(self, time, prices: np.ndarray):
+        
+        window = prices[max(0, time - self.lookback_period + 1):time + 1]
+        roll_max = np.max(window)
+        roll_min = np.min(window)
 
-            roll_max = np.full_like(prices, np.nan)
-            roll_min = np.full_like(prices, np.nan)
+        return roll_max, roll_min
 
-            for idp in range(min_periods - 1, len(prices)):
-                window = prices[max(0, idp - lookback + 1):idp + 1]
-                roll_max[idp] = np.max(window)
-                roll_min[idp] = np.min(window)
+    @staticmethod
+    def numpy_ewma(data, window):
+        """https://stackoverflow.com/questions/42869495/numpy-version-of-exponential-weighted-moving-average-equivalent-to-pandas-ewm"""
+        alpha = 2 /(window + 1.0)
+        alpha_rev = 1-alpha
+        n = data.shape[0]
 
-            return roll_max, roll_min
+        pows = alpha_rev**(np.arange(n+1))
 
-        def numpy_ewma(data, window):
-            """https://stackoverflow.com/questions/42869495/numpy-version-of-exponential-weighted-moving-average-equivalent-to-pandas-ewm"""
-            alpha = 2 /(window + 1.0)
-            alpha_rev = 1-alpha
-            n = data.shape[0]
+        scale_arr = 1/pows[:-1]
+        offset = data[0]*pows[1:]
+        pw0 = alpha*alpha_rev**(n-1)
 
-            pows = alpha_rev**(np.arange(n+1))
-
-            scale_arr = 1/pows[:-1]
-            offset = data[0]*pows[1:]
-            pw0 = alpha*alpha_rev**(n-1)
-
-            mult = data*pw0*scale_arr
-            cumsums = mult.cumsum()
-            out = offset + cumsums*scale_arr[::-1]
-            return out
-
-        roll_max, roll_min = calculate_rolling_extremes(self, self._env.unwrapped.price_data)
+        mult = data*pw0*scale_arr
+        cumsums = mult.cumsum()
+        out = offset + cumsums*scale_arr[::-1]
+        return out
+    
+    def act(self, state: np.ndarray) -> np.array:
+        time = self._env.unwrapped.time
+        prices = self._env.unwrapped.price_data
+        roll_max, roll_min = self.calculate_rolling_extremes(time, prices)
         roll_mean = (roll_max + roll_min) / 2.0
-
-        output = 40.0 * ((self._env.unwrapped.price_data - roll_mean) / (roll_max - roll_min))
-        smoothed_output = numpy_ewma(output, self.smooth)
-        return smoothed_output
+        output = 40.0 * ((prices[time] - roll_mean) / (roll_max - roll_min))
+        # smoothed_output = self.numpy_ewma(output, self.smooth)
+        return np.array([output.item()])
