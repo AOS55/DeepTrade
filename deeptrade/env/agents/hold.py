e4210df84dad835a1ce4ddb1731faa07ff192c71
quessy <aq15777@bristol.ac.uk>
2024-10-23 17:57:25 +0100

    edit hold rule to include MultiInstrumentEnv

--- deeptrade/env/agents/hold.py
+++ deeptrade/env/agents/hold.py
@@ -1,6 +1,6 @@
+from typing import Optional
 import gymnasium as gym
 import numpy as np
-
 
 class HoldAgent:
 
@@ -8,15 +8,38 @@ class HoldAgent:
 
     def __init__(self,
                  env: gym.Env,
-                 pos_size: float = 5.0):
+                 pos_size: float = 5.0,
+                 instrument: Optional[int] = None):
 
         self._env = env
+        self._env_type = type(env.unwrapped)
+        self.instrument = instrument
         self.pos_size = pos_size
 
-    def act(self, state: np.ndarray) -> int:
-        if state[-2] < self.pos_size:
-            action = self.pos_size - state[-2]
-            action = min(action, self._env.action_space.high[0])
+    def act(self, state: np.ndarray) -> np.ndarray:
+        from deeptrade.env import MultiInstrumentEnv
+        if self._env_type == MultiInstrumentEnv:
+            actions = np.zeros_like(state["positions"])
+            if self.instrument is None:
+                for idp in range(len(state["positions"])):
+                    if state["positions"][idp] < self.pos_size:
+                        action = self.pos_size - state["positions"][idp]
+                        action = min(action, self._env.action_space.high[idp])
+                    else:
+                        action = 0.0
+                    actions[idp] = action
+            else:
+                if state["positions"][self.instrument] < self.pos_size:
+                    action = self.pos_size - state["positions"][self.instrument]
+                    action = min(action, self._env.action_space.high[self.instrument])
+                else:
+                    action = 0.0
+                actions[self.instrument] = action
+            return actions
         else:
-            action = 0.0
-        return np.array([action])
+            if state[-2] < self.pos_size:
+                action = self.pos_size - state[-2]
+                action = min(action, self._env.action_space.high[0])
+            else:
+                action = 0.0
+            return np.array([action])
