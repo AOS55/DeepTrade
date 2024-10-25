f498448173255b5964237a28e00f3275140d80bb
quessy <aq15777@bristol.ac.uk>
2024-10-25 21:01:13 +0100

    change to target time_series processes

--- deeptrade/env/multi_instrument.py
+++ deeptrade/env/multi_instrument.py
@@ -8,7 +8,10 @@ from deeptrade.util.finance import calculate_simple_returns
 
 class Account:
 
-    def __init__(self, cash: float, n_instruments: int, positions: Optional[List[float]] = None):
+    def __init__(self,
+                 cash: float,
+                 n_instruments: int,
+                 positions: Optional[List[float]] = None):
         self._margin = cash
         self._n_instruments = n_instruments
         if positions is None:
@@ -38,23 +41,29 @@ class MultiInstrumentEnv(gym.Env):
     def __init__(self,
                  n_instruments: int = 3,
                  prices_data: Optional[np.ndarray] = None,
-                 period: int = 1, 
+                 period: int = 1,
                  starting_cash: float = 1000.0,
                  start_time: int = 11,
                  window: int = 10,
                  end_time: Optional[int] = None,
                  seed: Optional[int] = None,
+                 dt: float = 1.0,
                  price_gen_info: dict = {
-                     "starting_prices": [0.0, 0.0, 0.0],
-                     "means": [0.0, 0.0, 0.0],
-                     "vars": [1.0, 0.5, 0.2],
-                     "n_days": 1000}):
+                     "name": "GBM",
+                     "S0": np.array([100, 150, 200]),
+                     "mu": np.array([0.05, 0.07, 0.04]),
+                     "cov_matrix": np.array([[1.0, 0.5, 0.3], [0.5, 1.0, 0.4], [0.3, 0.4, 1.0]]),
+                     "dt": 1/252,
+                     "n_steps": 1000}):
         
         super().reset(seed=seed)
         if window > start_time-1:
             raise ValueError(f"window {window} must be less than start time {start_time}")
         if window < period:
             raise ValueError(f"window {window} must be greater than period {period}")
+        
+        self.period = period
+        self.dt = dt  # Update frequency of the price_data measured in fractions of a day
         
         # If price data is not provided, create price data
         if prices_data is None:
@@ -74,7 +83,6 @@ class MultiInstrumentEnv(gym.Env):
         self.account = Account(cash=self._starting_cash, n_instruments=self._n_instruments)
         self.time = self._start_time
         self.prices = self._observe_price_data(self.time)
-        self.period = period
         self.update_state()
         
         self.observation_space = spaces.Dict({
@@ -137,17 +145,18 @@ class MultiInstrumentEnv(gym.Env):
         return self.state, {}
     
     def _create_prices_data(self, price_gen_info: dict) -> np.ndarray:
-        """Create price data from random walk"""
-        y_data = np.array(price_gen_info["starting_prices"])
-        
-        # Advance once to get shapes right
-        y1_data = y_data + self.np_random.normal(price_gen_info["means"], price_gen_info["vars"])
-        y_data = np.column_stack([y_data, y1_data])
-        
-        for _ in range(price_gen_info["n_days"]):
-            y1_data = y_data[:, -1] + (y_data[:, -1] * self.np_random.normal(price_gen_info["means"], price_gen_info["vars"]))
-            y_data = np.column_stack([y_data, y1_data])
-        return y_data
+        """Create price data"""
+        from deeptrade.models import GBM, OU, JDM 
+        if price_gen_info["name"] == "GBM":
+            generator = GBM(**price_gen_info)
+        elif price_gen_info["name"] == "OU":
+            generator = OU(**price_gen_info)
+        elif price_gen_info["name"] == "JDM":
+            generator = JDM(**price_gen_info)
+        else:
+            raise ValueError(f"Time Series process {price_gen_info['name']} not found")
+
+        return generator.generate(self.dt, price_gen_info["n_steps"])
     
     def _observe_price_data(self, time: int) -> np.ndarray:
         return np.array(self.prices_data)[:, time-self._window:time+1]
